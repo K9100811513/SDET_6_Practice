d9ca836cc36af2462ea4b4a1bc5f040044eef446
Kalpana <Kalpana@LAPTOP-9MGQ1891>
2022-05-05 16:32:19 +0530

    bbb

--- src/GithubPractice/ArmstrongNumber.java
+++ src/GithubPractice/ArmstrongNumber.java
@@ -3,8 +3,25 @@ package GithubPractice;
 public class ArmstrongNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int num=407,rem,arm=0;
+		int cp=num;
+		while(num>0)
+		{
+		  rem=num%10;
+		  num=num/10;
+		  arm=arm+rem*rem*rem;
+		}
+		if(arm==cp)
+		{
+			System.out.println(cp+" Is Armstrong Number");
+		}
+		else
+		{
+			System.out.println(cp+" Not Armstrong Number");
+		}
+	}
 
 	}
 
-}
+
--- src/GithubPractice/PalindromeNumber.java
+++ src/GithubPractice/PalindromeNumber.java
@@ -3,8 +3,25 @@ package GithubPractice;
 public class PalindromeNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int num=989,rev=0,rem;
+		int cp=num;
+		while(num>0)
+		{
+			rem=num%10;
+			num=num/10;
+			rev=rev*10+rem;
+		}
+		System.out.println("Started");
+		
+		
+   if(cp==rev)
+   {
+	System.out.println("is Palindorm number");
+   }
+   else
+  {
+	System.out.println("is not Palindorm number");}
+	 }
 	}
 
-}
